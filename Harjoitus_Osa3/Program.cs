ce98e3712e82e309c6e46a45294907e5e9629eea
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2024-01-21 22:57:51 +0200

    Add project files.

---
+++ Harjoitus_Osa3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_Osa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tehtävä 2.
+            Vehicle audi = new Vehicle("100", "Audi");
+
+            // tehtävä 5.
+            audi.Accelerate();
+
+            // tehtävä 6.
+            audi.Brake();
+
+            // tehtävä 10.
+            Console.WriteLine(audi.Model.ToString());
+
+            Console.WriteLine(audi.Manufactor.ToString());
+
+        }
+    }
+}
---
+++ Harjoitus_Osa3/Vehicle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_Osa3
+{
+    internal class Vehicle
+    {
+        // tehtävä 6.
+        /// <summary>
+        /// Model Field
+        /// </summary>
+        private string model;
+
+        // tehtävä 7.
+        /// <summary>
+        /// Manufactor Field
+        /// </summary>
+        private string manufactor;
+
+        /// <summary>
+        /// Get for model
+        /// </summary>
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        /// <summary>
+        /// Get for manufactor
+        /// </summary>
+        public string Manufactor
+        {
+            get { return this.manufactor; }
+            set { this.manufactor = value; }
+        }
+
+        // tehtävä 8.
+        /// <summary>
+        /// Default Contructor
+        /// </summary>
+        public Vehicle(string model, string manufactor)
+        {
+            this.model = model;
+            this.manufactor = manufactor;
+        }
+
+        // tehtävä 3.
+        /// <summary>
+        /// Vehicle Accelerate
+        /// </summary>
+        public void Accelerate()
+        {
+            Console.WriteLine("Accelerate");
+        }
+
+        // tehtävä 4.
+        /// <summary>
+        /// Vehicle Brake
+        /// </summary>
+        public void Brake()
+        {
+            Console.WriteLine("Brake");
+        }
+    }
+}
